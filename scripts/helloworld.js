af8bda38f1f36063aeba2aa288c476c67e68eeb8
Lucas Melo <lwcmelo@gmail.com>
2019-04-23 00:52:28 +0200

    hello world stuff. looking neat :D

--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -3,26 +3,57 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
 
+container = document.createElement( 'div' );
+document.body.appendChild( container );
+
+var container = document.getElementById("helloworld_container");
+
 init();
 animate();
 
-function init() {
+// ---------------------------------------------------------------------------------------------------------------------
+// Function definitions
+// ---------------------------------------------------------------------------------------------------------------------
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+function init() {
+    //var width = window.innerWidth;
+    //var height = window.innerHeight;
+    //var width = container.offsetWidth;
+    //var height = container.offsetHeight;
+    var width = getWidth();
+    var height = getHeight();
+
+    console.log("Width: " + width + " | Height: " + height);
+
+    camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
     camera.position.z = 1;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     material = new THREE.MeshNormalMaterial();
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize(width, height);
 
+    container.appendChild( renderer.domElement );
+
+    // Register events
+    window.addEventListener("resize", resize, false);
+}
+
+function resize() {
+    var width = getWidth();
+    var height = getHeight();
+    console.log("Resized to " + width + " x " + height);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(width, height);
 }
 
 function animate() {
@@ -34,4 +65,14 @@ function animate() {
 
     renderer.render( scene, camera );
 
+}
+
+function getWidth() {
+    //return container.offsetWidth;
+    return window.innerWidth;
+}
+
+function getHeight() {
+    //return container.offsetHeight;
+    return window.innerHeight;
 }
