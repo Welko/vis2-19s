e93a655d809e6a3ae471a4f1d4d20f5387c4908f
Dominik Scholz <dominik.scholz@tuwien.ac.at>
2019-05-31 01:03:38 +0200

    added earth texture

--- scripts/scene_objects.js
+++ scripts/scene_objects.js
@@ -4,11 +4,7 @@ function fillSceneObjects(scene) {
     scene.add(new THREE.PolarGridHelper(100, 36, 10, 64, 0xcc5555, 0xcc5555));
 
     // earth
-    var earth_geometry = new THREE.SphereGeometry( 1, 48, 24 );
-    earth_geometry.scale(6.378137, 6.356752, 6.378137); // earth is ellipsoid: https://en.wikipedia.org/wiki/Figure_of_the_Earth#Volume
-    var earth_material = new THREE.MeshStandardMaterial({color: 0xff0000});
-    earth = new THREE.Mesh(earth_geometry, earth_material);
-    scene.add(earth);
+    scene.add(generateEarth());
 
     // moon
     var moon_geometry = new THREE.SphereGeometry( 1, 48, 24 );
@@ -52,4 +48,24 @@ function fillSceneObjects(scene) {
     skyBox.rotateX(Math.PI*0.5);
     skyBox.renderDepth = 1000.0;  
     scene.add(skyBox);  
+}
+
+function generateEarth() {
+    var earth_geometry = new THREE.SphereGeometry( 1, 48, 24 );
+    earth_geometry.scale(6.378137, 6.356752, 6.378137); // earth is ellipsoid: https://en.wikipedia.org/wiki/Figure_of_the_Earth#Volume
+    var earth_bump = new THREE.TextureLoader().load('./resources/earth_bump.png');
+    //var earth_oceans = new THREE.TextureLoader().load('./resources/oceans.png');
+    var earth_material = new THREE.MeshStandardMaterial({
+        map: new THREE.TextureLoader().load('./resources/earth.jpg'),
+        bumpMap: earth_bump,
+        bumpScale: 0.1,
+        roughnessMap: earth_bump,
+        metalnessMap: earth_bump,
+        //roughness: 0.5
+        // specular: 0x222222,
+        // shininess: 25
+
+    });
+    earth = new THREE.Mesh(earth_geometry, earth_material);
+    return earth;
 }
