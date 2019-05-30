743726b6ce3c917014abf6153ea035365511ac67
Dominik Scholz <dominik.scholz@tuwien.ac.at>
2019-05-30 20:23:39 +0200

    outsourced scene stuff, cleaned up main

--- scripts/main.js
+++ scripts/main.js
@@ -85,24 +85,6 @@ function animate() {
   render();
 }
 
-// function updateSatellitePositionAngular(delta, position) {
-//   for (var i = 0, p = 0; i < satellite_info.length; i+=5, p+=3) {
-
-//     satellite_info[i+1] += delta * satellite_info[i+3];
-//     satellite_info[i+2] += delta * satellite_info[i+4];
-
-//     var r = satellite_info[i];
-//     var phi = satellite_info[i+1];
-//     var theta = satellite_info[i+2];
-
-//     position.array[p] = r * Math.sin(theta) * Math.cos(phi);
-//     position.array[p+1] = r * Math.sin(theta) * Math.sin(phi);
-//     position.array[p+2] = r * Math.cos(theta);
-//   }
-
-//   position.needsUpdate = true;
-// }
-
 function updateSatellitePosition(delta, position) {
   var positions = position.array;
   for (var i = 0, p = 0; i < satellite_velocities.length; i++) {
@@ -156,50 +138,16 @@ function render() {
 
 function fillScene() {
   scene = new THREE.Scene();
-  scene.fog = new THREE.Fog(0x050505, 2000, 3500);
   scene.add(camera);
+
+  scene.fog = new THREE.Fog(0x050505, 2000, 3500);
 
   // lights
   scene.add(new THREE.AmbientLight(0x222222));
-
   var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
   scene.add(directionalLight);
 
-  // grid
-  scene.add(new THREE.GridHelper(1000, 10));
-  scene.add(new THREE.PolarGridHelper(100, 36, 10, 64, 0xcc5555, 0xcc5555));
-
-  // earth
-  var earth_geometry = new THREE.SphereGeometry( 1, 48, 24 );
-  earth_geometry.scale(6.378137, 6.356752, 6.378137); // earth is ellipsoid: https://en.wikipedia.org/wiki/Figure_of_the_Earth#Volume
-  var earth_material = new THREE.MeshStandardMaterial({color: 0xff0000});
-  earth = new THREE.Mesh(earth_geometry, earth_material);
-  scene.add(earth);
-
-  // moon
-  var moon_geometry = new THREE.SphereGeometry( 1, 48, 24 );
-  moon_geometry.scale(1.7381, 1.7360, 1.7381); // https://en.wikipedia.org/wiki/Moon
-  moon_geometry.translate(384.402, 0, 0);
-  var moon_material = new THREE.MeshStandardMaterial({color: 0xdddddd});
-  moon = new THREE.Mesh(moon_geometry, moon_material);
-  scene.add(moon);
-
-  // try plotting moon orbit:
-  var curve = new THREE.EllipseCurve(
-      0,  0,            // ax, aY
-      384, 400,           // xRadius, yRadius
-      0,  2 * Math.PI,  // aStartAngle, aEndAngle
-      false,            // aClockwise
-      0                // aRotation
-  );
-
-  var points = curve.getPoints( 50 );
-  var geometry = new THREE.BufferGeometry().setFromPoints(points);
-  geometry.rotateX(Math.PI * 0.67);
-  var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-  // Create the final object to add to the scene
-  var ellipse = new THREE.Line(geometry, material);
-  scene.add(ellipse);
+  fillSceneObjects(scene);
 
   //scene.add(generateRandomSatellites());
   generateSatellites(function(satPoints) {
@@ -264,8 +212,8 @@ function generateSatellites(callback) {
                 mainColor: { value: new THREE.Color( 0xffffff ) },
                 texture: { value: new THREE.TextureLoader().load("./resources/circle.png") }
             },
-            vertexShader: vertexShader,
-            fragmentShader: fragmentShader,
+            vertexShader: satellite_vert,
+            fragmentShader: satellite_frag,
             alphaTest: 0.9
         } );
 
@@ -273,73 +221,6 @@ function generateSatellites(callback) {
         callback(points);
 
     });
-}
-
-function generateRandomSatellites() {
-  var particles = 10000;
-  var geometry = new THREE.BufferGeometry();
-  var positions = [];
-  var colors = [];
-  var sizes = [];
-
-  satellite_info = [];
-  
-  var color = new THREE.Color();
-  
-  var min_r = 30;
-  var max_r = 40;
-  var max_phi = 2*Math.PI;
-  var max_theta = Math.PI;
-  var max_angular_speed = 0.01 * Math.PI * 2;
-
-  for ( var i = 0; i < particles; i ++ ) {
-    // positions
-    var r = Math.random() * (max_r - min_r) + min_r;
-    var phi = Math.random() * max_phi;
-    var theta = Math.random() * max_theta;
-
-    var velocity_phi = ((Math.random() * 2) - 1) * max_angular_speed;
-    var velocity_theta = ((Math.random() * 2) - 1) * max_angular_speed;
-    satellite_info.push(r, phi, theta, velocity_phi, velocity_theta);
-
-    var x = r * Math.sin(theta) * Math.cos(phi);
-    var y = r * Math.sin(theta) * Math.sin(phi);
-    var z = r * Math.cos(theta);
-    positions.push( x, y, z );
-    // colors
-    var vx = ( x / max_r ) + 0.5;
-    var vy = ( y / max_r ) + 0.5;
-    var vz = ( z / max_r ) + 0.5;
-    color.setRGB( vx, vy, vz );
-    colors.push( color.r, color.g, color.b );
-
-    sizes.push(PARTICLE_SIZE);
-  }
-
-  geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3).setDynamic(true));
-  geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3).setDynamic(true));
-  geometry.addAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setDynamic(true));
-  geometry.computeBoundingSphere(); // used?
-  //
-  // var material = new THREE.PointsMaterial({ 
-  //   size: 1, 
-  //   vertexColors: THREE.VertexColors,
-  //   map: new THREE.TextureLoader().load( 'circle.png' )} );
-
-  var material = new THREE.ShaderMaterial( {
-    uniforms: {
-      mainColor: { value: new THREE.Color( 0xffffff ) },
-      texture: { value: new THREE.TextureLoader().load("circle.png") }
-    },
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    alphaTest: 0.9
-  } );
-
-  // material.blending = THREE.AdditiveBlending;
-  
-  satellites = new THREE.Points(geometry, material);
-  return satellites;
 }
 
 let color_pastel_prev = -1;
