e09347ec9cbb2014ba06549784b05e3ed06c8a2f
Lucas Melo <lwcmelo@gmail.com>
2019-05-30 21:53:02 +0200

    moved some three.js stuff to satellite-manager.js

--- scripts/main.js
+++ scripts/main.js
@@ -9,16 +9,11 @@ var satellite_nameplate;
 
 var mouse, mouse_screen, raycaster, intersects, INTERSECTED;
 
-var PARTICLE_SIZE = 2; // remove later
+var PARTICLE_SIZE = 2; // TODO remove later
 
 var clock = new THREE.Clock();
 
 var earth, moon;
-
-var satellites;
-var satellite_info;
-var satellite_velocities;
-// var satellite_info; //remove later
 
 function init() {
 
@@ -85,26 +80,17 @@ function animate() {
   render();
 }
 
-function updateSatellitePosition(delta, position) {
-  let positions = position.array;
-  for (let i = 0; i < satellite_velocities.length; i++) {
-    positions[i] += delta * satellite_velocities[i];
-  }
-
-  position.needsUpdate = true;
-}
-
 function render() {
   let delta = clock.getDelta();
 
   cameraControls.update(delta);
 
-  if (satellites != null) {
+  if (_sats_points != null) {
     // satellite raycast (change size)
-    let geometry = satellites.geometry;
+    let geometry = _sats_points.geometry;
     let attributes = geometry.attributes;
     raycaster.setFromCamera(mouse, camera);
-    intersects = raycaster.intersectObject(satellites);
+    intersects = raycaster.intersectObject(_sats_points);
     if (intersects.length > 0) {
 
       // selected new satellite
@@ -118,7 +104,7 @@ function render() {
         attributes.size.array[INTERSECTED] = PARTICLE_SIZE * 1.5;
         attributes.size.needsUpdate = true;
 
-        satellite_nameplate.innerHTML = satellite_info[INTERSECTED].name;
+        satellite_nameplate.innerHTML = _sats[INTERSECTED].name;
         satellite_nameplate.style.left = mouse_screen.x + "px";
         satellite_nameplate.style.top = mouse_screen.y + "px";
       }
@@ -129,7 +115,7 @@ function render() {
       satellite_nameplate.innerHTML = "";
     }
 
-    updateSatellitePosition(delta, attributes.position);
+    updateSatellites(delta);
   }
 
   // render scene
@@ -150,88 +136,10 @@ function fillScene() {
 
   fillSceneObjects(scene);
 
-  //scene.add(generateRandomSatellites());
-  generateSatellites(function(satPoints) {
-    scene.add(satPoints);
-    satellites = satPoints;
+  getSatellites(function() {
+    scene.add(_sats_points);
   });
 }
-
-function generateSatellites(callback) {
-    getSatellites(function(sats) {
-
-        let positions = [];
-        let colors = [];
-        let sizes = [];
-
-        satellite_velocities = [];
-        satellite_info = sats;
-
-        let typesCache = [];
-        let colorsCache = [];
-
-        for (let i = 0; i < sats.length; i++) {
-            let sat = sats[i];
-            let index = typesCache.indexOf(sat.type);
-            if (index === -1) {
-                typesCache.push(sat.type);
-                colorsCache.push(next_color());
-                index = typesCache.length - 1;
-            }
-
-            let f = .001;
-            let x = sat.pos.x * f;
-            let y = sat.pos.z * f;
-            let z = sat.pos.y * f;
-            if (i < 10) console.log(x,y,z);
-            if (isNaN(x) || isNaN(y) || isNaN(z)) {
-              console.error("NaN in satellite position: ", i, x, y, z, sat);
-              x = 0.0;
-              y = 0.0;
-              z = 0.0;
-            }
-
-            positions.push(x,y,z);
-            c = colorsCache[index];
-            colors.push(c[0], c[1], c[2]);
-            sizes.push(PARTICLE_SIZE);
-
-            f *= 100;
-            satellite_velocities.push(sat.vel.x*f, sat.vel.y*f, sat.vel.z*f);
-        }
-
-        console.log("Loaded " + typesCache.length + " different types of satellites!");
-
-        let geometry = new THREE.BufferGeometry();
-        geometry.addAttribute('position', new THREE.Float32BufferAttribute(positions, 3).setDynamic(true));
-        geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3).setDynamic(true));
-        geometry.addAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setDynamic(true));
-        //geometry.computeBoundingSphere();
-
-        let material = new THREE.ShaderMaterial( {
-            uniforms: {
-                mainColor: { value: new THREE.Color( 0xffffff ) },
-                texture: { value: new THREE.TextureLoader().load("./resources/circle.png") }
-            },
-            vertexShader: satellite_vert,
-            fragmentShader: satellite_frag,
-            alphaTest: 0.9
-        } );
-
-        let points = new THREE.Points( geometry, material );
-        callback(points);
-
-    });
-}
-
-let color_satellites_prev = -1;
-let color_satellites = [
-    [0.89411765, 0.10196078, 0.10980392],
-    [0.21568627, 0.49411765, 0.72156863],
-    [0.30196078, 0.68627451, 0.29019608],
-    [0.59607843, 0.30588235, 0.63921569]
-];
-function next_color() { return color_satellites[(++color_satellites_prev) % color_satellites.length] }
 
 window.addEventListener('load', function() {
   init();
--- scripts/satellite-manager.js
+++ scripts/satellite-manager.js
@@ -1,6 +1,12 @@
 // Source: https://github.com/shashwatak/satellite-js
 
-let _satellites = null;
+let _sats = null;
+let _sats_pos = null;
+let _sats_vel = null;
+
+let _sats_points = null;
+
+let _scaling_factor = 0.001;
 
 function getSatellites(callback) {
     if (areSatellitesGood()) {
@@ -19,54 +25,144 @@ function fetchSatellites(callback, address = null) {
             return response.json();
         })
         .then(function(satList) {
-            callback(buildSatellites(satList));
+            buildSatellites(satList);
+            callback();
         });
 }
 
 function areSatellitesGood() {
-    return _satellites != null;
+    return _sats != null;
 }
 
+// Source: https://github.com/shashwatak/satellite-js
 function buildSatellites(satList) {
     let now = new Date();
 
     // Greenwich Mean Sidereal Time: https://en.wikipedia.org/wiki/Sidereal_time#Definition
     let gmst = satellite.gstime(now);
 
+    let colors = [];
+    let sizes = [];
+
+    let typesCache = [];
+    let colorsCache = [];
+
     let sats = [];
+    let poss = [];
+    let vels = [];
     for (let i = 0; i < satList.length; i++) {
-        let sat = buildSatellite(satList[i], now, gmst);
-        if (sat != null) {
-            sats.push(sat);
+
+        let sati = satList[i];
+
+        let satrec = satellite.twoline2satrec(sati.TLE_LINE1, sati.TLE_LINE2); // Satellite record
+
+        // ECI: https://en.wikipedia.org/wiki/Earth-centered_inertial
+        let positionAndVelocity_eci = satellite.propagate(satrec, now);
+
+        let posEcf, velEcf;
+        if (!positionAndVelocity_eci.hasOwnProperty('position') ||
+            !positionAndVelocity_eci.hasOwnProperty('velocity') ||
+            isNaN(positionAndVelocity_eci.position.x) ||
+            isNaN(positionAndVelocity_eci.velocity.x) ||
+            typeof positionAndVelocity_eci.position !== "object" ||
+            typeof positionAndVelocity_eci.velocity !== "object"
+        ) {
+            //console.log("Type of position: " + typeof positionAndVelocity_eci.position);
+            //console.log("Is object? " + (typeof positionAndVelocity_eci.position === "object"));
+            posEcf = {x: 0, y: 0, z: 0};
+            velEcf = {x: 0, y: 0, z: 0};
+        } else {
+            posEcf = satellite.eciToEcf(positionAndVelocity_eci.position, gmst);
+            velEcf = satellite.eciToEcf(positionAndVelocity_eci.velocity, gmst);
+
+            posEcf.x *= _scaling_factor;
+            posEcf.y *= _scaling_factor;
+            posEcf.z *= _scaling_factor;
+
+            let f = _scaling_factor * 100;
+            velEcf.x *= f;
+            velEcf.y *= f;
+            velEcf.z *= f;
         }
+
+        let sat = {
+            intldes:  sati.INTLDES,// LM: I couldn't find what this means :/
+            name: sati.OBJECT_NAME,
+            type: sati.OBJECT_TYPE
+        };
+
+        // Debug TODO: remove
+        if (i < 10) console.log(posEcf.x, posEcf.y, posEcf.z);
+        if (isNaN(posEcf.x) || isNaN(posEcf.y) || isNaN(posEcf.z)) {
+            console.error("NaN in satellite position: ", i, posEcf.x, posEcf.y, posEcf.z, sat);
+            posEcf.x = 0.0;
+            posEcf.y = 0.0;
+            posEcf.z = 0.0;
+        }
+
+        // three.js stuff
+        let index = typesCache.indexOf(sat.type);
+        if (index === -1) {
+            typesCache.push(sat.type);
+            colorsCache.push(next_color());
+            index = typesCache.length - 1;
+        }
+        let c = colorsCache[index];
+
+        sats.push(sat);
+        poss.push(posEcf.x, posEcf.y, posEcf.z);
+        vels.push(velEcf.x, velEcf.y, velEcf.z);
+
+        colors.push(c[0], c[1], c[2]);
+        sizes.push(PARTICLE_SIZE);
     }
 
-    _satellites = sats;
-    return sats;
+    let geometry = new THREE.BufferGeometry();
+    geometry.addAttribute('position', new THREE.Float32BufferAttribute(poss, 3).setDynamic(true));
+    geometry.addAttribute('color', new THREE.Float32BufferAttribute(colors, 3).setDynamic(true));
+    geometry.addAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setDynamic(true));
+    //geometry.computeBoundingSphere();
+
+    let material = new THREE.ShaderMaterial( {
+        uniforms: {
+            mainColor: { value: new THREE.Color(0xffffff) },
+            texture: { value: new THREE.TextureLoader().load("./resources/circle.png") }
+        },
+        vertexShader: satellite_vert,
+        fragmentShader: satellite_frag,
+        alphaTest: 0.9
+    } );
+
+    let points = new THREE.Points(geometry, material);
+
+    _sats = sats;
+    _sats_pos = poss;
+    _sats_vel = vels;
+
+    _sats_points = points;
+
+    console.log("Loaded " + _sats.length + " satellites, " + typesCache.length + " types!")
 }
 
-// Source: https://github.com/shashwatak/satellite-js
-function buildSatellite(data, date, gmst) {
-    let satrec = satellite.twoline2satrec(data.TLE_LINE1, data.TLE_LINE2); // Satellite record
-
-    // ECI: https://en.wikipedia.org/wiki/Earth-centered_inertial
-    let positionAndVelocity_eci = satellite.propagate(satrec, date);
-
-    if (!positionAndVelocity_eci.hasOwnProperty('position') || !positionAndVelocity_eci.hasOwnProperty('velocity')) {
-        return null;
+function updateSatellites(delta) {
+    if (_sats_vel == null || _sats_pos == null) {
+        return;
     }
 
-    // ECF: https://en.wikipedia.org/wiki/ECEF
-    let positionEcf = satellite.eciToEcf(positionAndVelocity_eci.position, gmst);
-    let velocityEcf = satellite.eciToEcf(positionAndVelocity_eci.velocity, gmst);
+    let position = _sats_points.geometry.attributes.position;
+    let positions = position.array;
+    for (let i = 0; i < _sats_vel.length; i++) {
+        positions[i] += delta * _sats_vel[i];
+    }
 
-    return {
-        intldes: data.INTLDES, // LM: I couldn't find what this means :/
-        name: data.OBJECT_NAME,
-        type: data.OBJECT_TYPE,
-
-        // Position and velocity both on ECF coordinates
-        pos: positionEcf,
-        vel: velocityEcf
-    };
+    position.needsUpdate = true;
 }
+
+let color_satellites_prev = -1;
+let color_satellites = [
+    [0.89411765, 0.10196078, 0.10980392],
+    [0.21568627, 0.49411765, 0.72156863],
+    [0.30196078, 0.68627451, 0.29019608],
+    [0.59607843, 0.30588235, 0.63921569]
+];
+function next_color() { return color_satellites[(++color_satellites_prev) % color_satellites.length] }
